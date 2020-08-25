d19483750ab5162ea36b3aa35dad403cd40e4222
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-25 12:16:04 +0700

    Bot Response

--- src/bot.js
+++ src/bot.js
@@ -8,4 +8,14 @@ client.on("ready", () => {
 	console.log(`${client.user.tag} has logged in.`);
 });
 
+client.on("message", (message) => {
+	if (message.author.bot) {
+		return;
+	}
+	console.log(`[${message.author.tag}]: ${message.content}`);
+	if (message.content === "hello") {
+		message.reply("hello there !");
+	}
+});
+
 client.login(process.env.DISCORDJS_BOT_TOKEN);
