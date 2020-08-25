85f021e01a9d7d03b1bd36c9bd560d004a60b439
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-25 12:28:32 +0700

    Basic Chat commands

--- src/bot.js
+++ src/bot.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const { Client } = require("discord.js");
 
 const client = new Client();
+const PREFIX = "$";
 
 client.on("ready", () => {
 	console.log(`${client.user.tag} has logged in.`);
@@ -12,9 +13,13 @@ client.on("message", (message) => {
 	if (message.author.bot) {
 		return;
 	}
-	console.log(`[${message.author.tag}]: ${message.content}`);
-	if (message.content === "hello") {
-		message.reply("hello there !");
+	if (message.content.startsWith(PREFIX)) {
+		const [CMD_NAME, ...args] = message.content
+			.trim()
+			.substring(PREFIX.length)
+			.split(/\s+/);
+		console.log(CMD_NAME);
+		console.log(args);
 	}
 });
 
